1b5fcea7c5b21d266cbacd2b534a49ced67c4546
Noel Gilliland <noelgilliland03@gmail.com>
2026-02-08 16:16:55 -0800

    adding basic graphics/ workable fireball

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -11,6 +11,9 @@ public class PlayerAttack : MonoBehaviour
    
 
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+
     private Animator anim;
     private PlayerMovement playerMovement;
     private float cooldowntimer = Mathf.Infinity;
@@ -36,5 +39,19 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetTrigger("attack");
         cooldowntimer = 0;
+
+        //pool fireballs
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -10,9 +10,54 @@ public class Projectile : MonoBehaviour
         
     }
 
+
+    [SerializeField] private float speed;
+    private bool hit;
+    private float direction;
+    private float lifetime;
+
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if(hit) return;
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+        lifetime += Time.deltaTime;
+        if(lifetime > 3) gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+    public void SetDirection(float _direction)
+    {
+        lifetime = 0;
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if(Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
         
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+               gameObject.SetActive(false);
     }
 }
