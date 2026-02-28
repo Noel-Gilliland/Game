a31b592cc79e0d40e78cc1aa63f4ea06cbff5a11
Noel Gilliland <noelgilliland03@gmail.com>
2026-02-27 16:12:45 -0800

    Finished Traps Video.
    Finished making spikehead trap. Moving onto enemies

--- Assets/Scripts/Enemies/SpikeHead.cs
+++ Assets/Scripts/Enemies/SpikeHead.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class SpikeHead : EnemyDamage
 {
-    private Vector3 destination;
+    [Header("SpikeHead Attributes")]
     [SerializeField] private float speed;
     [SerializeField] private float range;
     [SerializeField] private float checkDelay;
+    [SerializeField] private LayerMask playerLayer;
+    private Vector3[] directions = new Vector3[4];
+    private Vector3 destination;
     private float checkTimer;
-
-
     private bool attacking;
 
-    private Vector3[] directions = new Vector3[4];
+    private void OnEnable()
+    {
+        Stop();
+    }
+
     private void Update()
     {
         //move spikehead to destination if attacking
@@ -21,24 +26,50 @@ public class SpikeHead : EnemyDamage
         {
                        //check if player is in range
             checkTimer += Time.deltaTime;
-            if (checkTimer > checkDelay)
+            if (checkTimer >= checkDelay)
             {
-                checkTimer = 0;
-                Collider2D hit = Physics2D.OverlapCircle(transform.position, range, LayerMask.GetMask("Player"));
-                if (hit != null)
-                {
-                    attacking = true;
-                    destination = (hit.transform.position - transform.position).normalized;
-                }
+                CheckForPlayer();
+                
             }
         }
     }
     private void CheckForPlayer()
     {
-        //check if spikehead sees player
+        CalculateDirections();
+
+        //check if spikehead sees player in all 4 directions
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Debug.DrawRay(transform.position, directions[i], Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, directions[i], range, playerLayer);
+
+            if(hit.collider != null && !attacking)
+            {
+                attacking = true;
+                destination = directions[i];
+                checkTimer = 0;
+            }
+        }
     }
     private void CalculateDirections()
     {
+        directions[0] = transform.right * range; //right direction
+        directions[1] = -transform.right * range; //left direction
+        directions[2] = transform.up * range; //up direction
+        directions[3] = -transform.up * range; //down direction
+    }
+
+    private void Stop()
+    {
+        destination = transform.position;
+        attacking = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+        //stop spikehead if it hits something
+
+        Stop();
 
     }
 }
